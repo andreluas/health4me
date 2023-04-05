a373857fa1d49a6557c2151396092390848ef4eb
andreluas <andreluas.k@gmail.com>
2023-04-05 17:42:15 -0300

    feat: init :tada:

---
+++ src/main/java/br/com/fiap/health4me/Health4meApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.health4me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Health4meApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Health4meApplication.class, args);
+	}
+
+}
