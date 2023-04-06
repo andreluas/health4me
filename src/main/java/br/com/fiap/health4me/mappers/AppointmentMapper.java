20ff96403c384ab0a65aa3cfc8abf3fa59b12bc7
andreluas <andreluas.k@gmail.com>
2023-04-06 13:22:32 -0300

    refact: mapper refactor

--- src/main/java/br/com/fiap/health4me/mappers/AppointmentMapper.java
+++ src/main/java/br/com/fiap/health4me/mappers/AppointmentMapper.java
@@ -3,6 +3,7 @@ package br.com.fiap.health4me.mappers;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import br.com.fiap.health4me.dtos.AppointmentDTO;
 import br.com.fiap.health4me.models.Appointment;
@@ -10,8 +11,10 @@ import br.com.fiap.health4me.models.Appointment;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
 public interface AppointmentMapper {
 
+    @Mapping(target = "register.email", source = "register")
     Appointment dtoToEntity(AppointmentDTO acccountDTO);
 
+    @Mapping(target = "register", source = "register.email")
     AppointmentDTO entityToDTO(Appointment Appointment);
 
     List<AppointmentDTO> entityListToDTOList(List<Appointment> Appointments);
